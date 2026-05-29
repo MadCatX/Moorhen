a777c4b0884c2a206fc2cae60125d4b24b14e6e1
Michal Malý <madcatxster@gmail.com>
2026-05-29 17:13:05 +0200

    Prep menu layout

--- baby-gru/src/components/snack-bars/PopupControls/ModifyNtC.tsx
+++ baby-gru/src/components/snack-bars/PopupControls/ModifyNtC.tsx
@@ -10,8 +10,88 @@ export const ModifyNtC = () => {
     return (
         <MoorhenStack direction="vertical">
             <div>Modify NtC</div>
-            <div>{molNo}</div>
-            <div>{chosenAtom.chain_id} {chosenAtom.res_name}</div>
+
+            <div>
+                <div>Assigned NtC</div>
+                <div></div>
+
+                <div>Closest NtC</div>
+                <div></div>
+
+                <div>RMSD of displayed NtC</div>
+                <div></div>
+
+                <div>Alt. conf</div>
+                <div>
+                    <select>
+                    </select>
+                </div>
+
+                <div>Displayed NtC</div>
+                <div>
+                    <select>
+                    </select>
+
+                    <button>Reset</button>
+                </div>
+            </div>
+
+            <div>Torsions &amp; distances</div>
+
+
+            <div>
+                <div></div>
+                <div>Actual (deg)</div>
+                <div>{'\u03B4'} to NtC (deg)</div>
+
+                <div>{'\u018D'}1</div>
+                <div></div>
+                <div></div>
+
+                <div>{'\u025B'}1</div>
+                <div></div>
+                <div></div>
+
+                <div>{'\u03B6'}1</div>
+                <div></div>
+                <div></div>
+
+                <div>{'\u03B1'}2</div>
+                <div></div>
+                <div></div>
+
+                <div>{'\u03B2'}2</div>
+                <div></div>
+                <div></div>
+
+                <div>{'\u03B3'}2</div>
+                <div></div>
+                <div></div>
+
+                <div>{'\u03B4'}2</div>
+                <div></div>
+                <div></div>
+
+                <div>{'\u03C7'}1</div>
+                <div></div>
+                <div></div>
+
+                <div>{'\u03C7'}2</div>
+                <div></div>
+                <div></div>
+
+                <div>C'C'</div>
+                <div></div>
+                <div></div>
+
+                <div>N'N'</div>
+                <div></div>
+                <div></div>
+
+                <div>{'\u00B5'}</div>
+                <div></div>
+                <div></div>
+            </div>
         </MoorhenStack>
     );
 }
